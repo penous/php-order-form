8c62325abbf64e98c498384ef25467b8eb1cbcd4
Tom Dirinck <tom.dirinck@gmail.com>
2021-05-21 17:08:49 +0200

    Added validation & values to fields.

--- form-view.php
+++ form-view.php
@@ -29,10 +29,12 @@
       <div class="form-row">
         <div class="form-group col-md-6">
           <label for="email">E-mail:</label>
-          <input type="text" id="email" name="email" class="form-control" />
+          <input type="text" id="email" name="email" class="form-control" value=<?php echo $email ?>>
+          <?php if (!empty($emailErr)): ?>
           <span class="alert alert-danger" role="alert">
             <p><?php echo $emailErr ?></p>
           </span>
+          <?php endif; ?>
         </div>
         <div></div>
       </div>
@@ -43,34 +45,43 @@
         <div class="form-row">
           <div class="form-group col-md-6">
             <label for="street">Street:</label>
-            <input type="text" name="street" id="street" class="form-control">
+            <input type="text" name="street" id="street" class="form-control" value=<?php echo $street ?>>
+            <?php if (!empty($streetErr)): ?>
             <span class="alert alert-danger" role="alert">
               <p><?php echo $streetErr ?></p>
             </span>
+            <?php endif; ?>
 
           </div>
           <div class="form-group col-md-6">
             <label for="streetnumber">Street number:</label>
-            <input type="text" id="streetnumber" name="streetnumber" class="form-control">
+            <input type="text" id="streetnumber" name="streetnumber" class="form-control"
+              value=<?php echo $streetNumber ?>>
+            <?php if (!empty($streetNumberErr)): ?>
             <span class="alert alert-danger" role="alert">
               <p><?php echo $streetNumberErr ?></p>
             </span>
+            <?php endif; ?>
           </div>
         </div>
         <div class="form-row">
           <div class="form-group col-md-6">
             <label for="city">City:</label>
-            <input type="text" id="city" name="city" class="form-control">
+            <input type="text" id="city" name="city" class="form-control" value=<?php echo $city ?>>
+            <?php if (!empty($cityErr)): ?>
             <span class="alert alert-danger" role="alert">
               <p><?php echo $cityErr ?></p>
             </span>
+            <?php endif ?>
           </div>
           <div class="form-group col-md-6">
             <label for="zipcode">Zipcode</label>
-            <input type="text" id="zipcode" name="zipcode" class="form-control">
+            <input type="text" id="zipcode" name="zipcode" class="form-control" value=<?php echo $zipcode ?>>
+            <?php if (!empty($zipcodeErr)): ?>
             <span class="alert alert-danger" role="alert">
               <p><?php echo $zipcodeErr ?></p>
             </span>
+            <?php endif ?>
           </div>
         </div>
       </fieldset>
