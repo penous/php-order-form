3ac2c42d446b70198dcfa95ef758c63a99301d9c
Tom Dirinck <tom.dirinck@gmail.com>
2021-05-21 17:08:14 +0200

    Completed form validation.

--- form-validation.php
+++ form-validation.php
@@ -1,5 +1,5 @@
 <?php
-$streetErr = $emailErr = $cityErr = $streetNumberErr = $zipcode = "Yolo swaggerino";
+$streetErr = $emailErr = $cityErr = $streetNumberErr = $zipcode = "";
 $street = $email = $city =  $streetNumber = $zipcode = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -7,30 +7,53 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $emailErr = "Email is required";
     } else {
         $email = test_input($_POST["email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format";
+        }
     }
 
     if (empty($_POST["street"])) {
         $streetErr = "Street is required";
     } else {
         $street = test_input($_POST["street"]);
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $street)) {
+            $streetErr = "Only letters and white space allowed";
+        } else {
+            $_SESSION["street"] = $street;
+        }
     }
 
-    if (empty($_POST["streetNumber"])) {
+    if (empty($_POST["streetnumber"])) {
         $streetNumberErr = "Streetnumber is required";
     } else {
-        $streetNumber = test_input($_POST["streetNumber"]);
+        $streetNumber = test_input($_POST["streetnumber"]);
+        if (!is_numeric($streetNumber)) {
+            $streetNumberErr = "Only numbers allowed";
+        } else {
+            $_SESSION["streetNumber"] = $streetNumber;
+        }
     }
 
     if (empty($_POST["city"])) {
         $cityErr = "city is required";
     } else {
         $city = test_input($_POST["city"]);
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $city)) {
+            $cityErr = "Only letters and white space allowed";
+        } else {
+            $_SESSION["city"] = $city;
+        }
     }
 
     if (empty($_POST["zipcode"])) {
         $zipcodeErr = "zipcode is required";
     } else {
         $zipcode = test_input($_POST["zipcode"]);
+        if (!is_numeric($zipcode)) {
+            return $zipcodeErr = "Only numbers allowed";
+        } else {
+            $_SESSION["zipcode"] = $zipcode;
+        }
     }
 }
 
